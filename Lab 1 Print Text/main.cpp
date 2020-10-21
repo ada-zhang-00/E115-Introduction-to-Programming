85c82cc7541ea2aa93f8ace9fd65f1c82e0019df
Ada Zhang <73206938+ada-zhang-00@users.noreply.github.com>
2020-10-20 22:55:55 -0400

    Create main.cpp

---
+++ Lab 1 Print Text/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Assignment 1: Print Text on the Console
+//
+//  Created by Ada Zhang on 9/14/20.
+//  Copyright © 2020 Ada Zhang. All rights reserved.
+//
+//Your first assignment is to print five messages of your choice on the output terminal. 
+//When you compile and run your program, the output should be those five messages. 
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    cout << "Hey! My name is Ada Zhang!\n";
+    cout << "I am junior\n";
+    cout << "business and technology major\n";
+    cout << "with the concentrations in marketing and computer science\n";
+    cout << "and a minor in quantitative finance\n";
+
+    return 0;
+}
